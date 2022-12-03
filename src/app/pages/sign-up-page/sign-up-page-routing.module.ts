d1bc4d0e4835fbc3bc3c76efaa93fa91dbc4f364
soks <soks145@gmail.com>
2022-12-03 17:15:31 +0500

    refactored header-page

--- src/app/pages/sign-up-page/sign-up-page-routing.module.ts
+++ src/app/pages/sign-up-page/sign-up-page-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInPageComponent } from '@pages/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from '@pages/sign-up-page/sign-up-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: SignInPageComponent
+    component: SignUpPageComponent
   }
 ];
 
