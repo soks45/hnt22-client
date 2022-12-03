0e966a2ece044e7d8e5faa7662e87b22838cd17d
soks <soks145@gmail.com>
2022-12-04 04:58:07 +0500

    fixed loading

--- src/app/ui/qr-code/qr-code.component.ts
+++ src/app/ui/qr-code/qr-code.component.ts
@@ -7,11 +7,8 @@ import { Component } from '@angular/core';
 })
 export class QrCodeComponent {
   saveAsImage(parent: any) {
-    const parentElement = parent.el.nativeElement.querySelector("img").src;
-
+    const parentElement = parent.qrcElement.nativeElement.querySelector("img").src;
     let blobData = this.convertBase64ToBlob(parentElement);
-
-    // @ts-ignore
     if (window.navigator && (window.navigator as any).msSaveOrOpenBlob) { //IE
       (window.navigator as any).msSaveOrOpenBlob(blobData, 'Qrcode');
     } else {
@@ -22,23 +19,16 @@ export class QrCodeComponent {
       link.download = 'Qrcode';
       link.click();
     }
-
   }
 
   private convertBase64ToBlob(Base64Image: any) {
-    // SPLIT INTO TWO PARTS
     const parts = Base64Image.split(';base64,');
-    // HOLD THE CONTENT TYPE
     const imageType = parts[0].split(':')[1];
-    // DECODE BASE64 STRING
     const decodedData = window.atob(parts[1]);
-    // CREATE UNIT8ARRAY OF SIZE SAME AS ROW DATA LENGTH
     const uInt8Array = new Uint8Array(decodedData.length);
-    // INSERT ALL CHARACTER CODE INTO UINT8ARRAY
     for (let i = 0; i < decodedData.length; ++i) {
       uInt8Array[i] = decodedData.charCodeAt(i);
     }
-    // RETURN BLOB IMAGE AFTER CONVERSION
     return new Blob([uInt8Array], { type: imageType });
   }
 }
