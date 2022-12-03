27b6bbe7b4937954a2d095b99808fce4b6b8a85b
soks <soks145@gmail.com>
2022-12-03 17:12:08 +0500

    fixed route signup

--- src/app/pages/sign-up-page/sign-up-page-routing.module.ts
+++ src/app/pages/sign-up-page/sign-up-page-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInPageComponent } from '@pages/sign-in-page/sign-in-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SignInPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
