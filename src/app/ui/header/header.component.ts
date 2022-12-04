acb8ea78d754c9d904c0e415da28ec4faedd1dc3
soks <soks145@gmail.com>
2022-12-04 06:43:19 +0500

    fixed loading

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,11 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { DestroyMixin } from '@mixins/destroy.mixin';
+import { BaseObject } from '@mixins/mixins';
 import { MenuItem } from '@models/menu-item';
+import { User } from '@models/user';
+import { AuthService } from '@services/auth.service';
+import { take, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'hnt22-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent extends DestroyMixin(BaseObject) {
+  user: User | null = null;
+  constructor(private auth: AuthService) {
+    super();
+
+    this.auth.user$.pipe(takeUntil(this.destroyed$)).subscribe(u => this.user = u);
+  }
   @Input() menuItems: MenuItem[] = [];
+
+  logout() {
+    this.auth.logout().subscribe();
+  }
 }
--- src/app/ui/header/header.module.ts
+++ src/app/ui/header/header.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { HeaderComponent } from './header.component';
 import { MenuModule } from '@ui/header/components/menu/menu.module';
 
@@ -7,10 +8,11 @@ import { MenuModule } from '@ui/header/components/menu/menu.module';
   declarations: [
     HeaderComponent
   ],
-  imports: [
-    CommonModule,
-    MenuModule
-  ],
+    imports: [
+        CommonModule,
+        MenuModule,
+        RouterLink
+    ],
   exports: [
     HeaderComponent
   ]
